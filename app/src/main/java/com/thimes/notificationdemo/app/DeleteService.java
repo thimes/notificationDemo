d2e555c40344401db6a559e871b462d63f258eef
Travis Himes <thimes@meetme.com>
2014-05-29 09:41:35 -0400

    android studio version

---
+++ app/src/main/java/com/thimes/notificationdemo/app/DeleteService.java
@@ -0,0 +1,36 @@
+package com.thimes.notificationdemo.app;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by thimes on 5/29/14.
+ */
+public class DeleteService extends IntentService {
+
+    private static final String EXTRA_NOTIFICATION_ID = "com.himes.demo.notifications.NOTIFICATION_ID";
+    private static final String TAG = "DeleteService";
+
+    public DeleteService() {
+        super("DeleteService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.v(TAG, "this is where the delete would happen...");
+        int notificationId = intent.getIntExtra(EXTRA_NOTIFICATION_ID, -1);
+        NotificationManager notifMgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notifMgr.cancel(notificationId);
+        // TODO: do more interesting things here, if you want to switch on the action
+    }
+
+    public static Intent createIntent(Context context, int notificationId) {
+        Intent intent = new Intent(context, DeleteService.class);
+        intent.putExtra(EXTRA_NOTIFICATION_ID, notificationId);
+        return intent;
+    }
+
+}
